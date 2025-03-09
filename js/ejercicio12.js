0accf52464e7dc945731f3e3c9c12a676d5250a7
lucas <lucasmtrachta90@gmail.com>
2025-03-09 18:26:51 -0300

    cuarto commit

--- js/ejercicio12.js
+++ js/ejercicio12.js
@@ -2,4 +2,4 @@ let miListaDePendientes = ["titanic","godzilla","kingkong","birdman"]
 let nuevaPelicula = prompt("ingresa una pelicula")
 miListaDePendientes.push("elpadrino")
 miListaDePendientes[1]="avengers"
-console.log(miListaDePendientes)
+console.log(miListaDePendientes)        
